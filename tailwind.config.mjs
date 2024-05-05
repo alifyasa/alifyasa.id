c67d27724ac38c5c42473780968de8fa288017d0
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-05-05 08:28:52 +0700

    feat: Change Font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,17 +7,20 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				'satoshi': [
-					'Satoshi', 'Helvetica', 'ui-sans-serif', 
+				'rubik': [
+					'Rubik', 'Helvetica', 'ui-sans-serif', 
 					'system-ui', 'sans-serif', "Apple Color Emoji", 
 					"Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"
 				],
 			},			
 			colors: {
-				'deep-black': '#0D0D0D',
-				'dark-gray': '#1F1F1F',
-				'light-gray': '#FAFAFA',
-				'eggshell': '#F0EAD6',
+				'background': '#ffffff',
+				'text': '#2e2e2e',
+				'primary': '#48b1ca',
+				'secondary': '#8fd4e4',
+				'primary': '#ec1e18',
+				'secondary': '#82f595',
+				'accent': '#007db3'
 			},
 		},
 	},
