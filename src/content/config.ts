cb1052d344fa1bbc524206af29952a7542df8091
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-10-04 07:03:35 +0700

    feat: Use Content Collection

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { z, defineCollection } from "astro:content";
+
+const postsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.string().transform((str) => new Date(str)),
+    })
+});
+
+export const collections = {
+  posts: postsCollection,
+};
